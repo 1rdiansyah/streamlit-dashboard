0eec0a082ccbc911d7becea63a9ab2961b2bb9f0
Irdiansyah <63138445+1rdiansyah@users.noreply.github.com>
2024-01-09 10:19:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pickle
 from PIL import Image
 import streamlit as st
+from streamlit_metrics import metric, metric_row
 import plotly.express as px
 
 px.defaults.template = 'plotly_dark'
